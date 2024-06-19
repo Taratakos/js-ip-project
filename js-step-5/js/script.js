0d327f69c5b7b57fa7d070df4a5cb0d80695efb6
Taras Zarytskyi''git config --global user.email taras.zarytskyi@gmail.com” <taras.zarytskyi@gmail.com>
2024-06-19 10:50:50 +0300

    complete step 5 | action with elements

---
+++ js-step-5/js/script.js
@@ -0,0 +1,48 @@
+/* Задания на урок:
+
+1) Видалити всі рекламні блоки зі сторінки (права частина сайту)
+
+2) Змінити жанр фільму, змінити "комедію" на "драму"
+
+3) Змінити задній фон постера з фільмом на зображення "bg.jpg". Воно лежить в папці img.
+Реалізувати тільки за допомогою JS
+
+4) Список фільмів на сторінці сформувати на основі даних із цього файлу JS.
+Відсортувати їх за алфавітом
+
+5) Додати нумерацію виведених фільмів */
+
+'use strict';
+
+const movieDB = {
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против..."
+  ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img');
+const poster = document.querySelector('.promo__bg')
+const genre = poster.querySelector('.promo__genre');
+const movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => item.remove());
+
+genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("./img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort()
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+      <div class="delete"></div>
+      </li >
+      `;
+});
